eaef67efa7a19dd5735d2ec4c00e746ba1caa188
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-10-15 12:49:04 -0300

    Set general styles and page structure Set html for the page structure, create main components and set style for header and footer

--- PokemonMemory/src/App.jsx
+++ PokemonMemory/src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import "./style.css"
+
 function App() {
 
   return (
     <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   )
 }
