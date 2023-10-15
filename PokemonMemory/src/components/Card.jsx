e3fae8257f40d7233c6fdaaa1c7ac0c27c0240ba
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-10-15 14:30:48 -0300

    Finish style and set html for cards

---
+++ PokemonMemory/src/components/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({pokemon}){
+    return(
+        <button className="pokemon-card">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png" alt="Pikachu"/>
+            <h2>Pikachu</h2>
+        </button>
+    )
+}
--- PokemonMemory/src/components/Main.jsx
+++ PokemonMemory/src/components/Main.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import Card from "./Card";
+
 export default function Main(){
+    
+    const [score, setScore] = useState(0);
+    const [maxScore, setMaxScore] = useState(0);
+    
     return(
         <main>
-            
+            <section id="cards-section">
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </section>
+            <section id="score-section">
+                <h2>Score: {score}</h2>
+                <h2>Max score: {maxScore}</h2>
+            </section>
         </main>
     )
 }
