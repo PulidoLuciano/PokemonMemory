de32f95d33e2b85925b8c9ad9bbc81baf2ccc349
Luciano Pulido <lucianonicolaspulido@gmail.com>
2023-10-15 19:19:18 -0300

    Shuffle cards in each round

--- PokemonMemory/src/components/Main.jsx
+++ PokemonMemory/src/components/Main.jsx
@@ -1,6 +1,24 @@
 import { useEffect, useState } from "react"
 import Card from "./Card";
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 export default function Main(){
     
     const [score, setScore] = useState(0);
@@ -11,7 +29,7 @@ export default function Main(){
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokedex/2").then(res => res.json())
         .then(data => {
-            setPokemon(data.pokemon_entries.slice(0,8));
+            setPokemon(data.pokemon_entries.splice(0,8));
         });    
     }, []);
 
@@ -19,6 +37,7 @@ export default function Main(){
         let wonDialog = document.getElementById("wonDialog");
         if(score == pokemon.length && pokemon.length) wonDialog.showModal();
         if(score > maxScore) setMaxScore(score);
+        setPokemon(shuffle(pokemon));
     }, [score])
 
     function handleCardClick(event){
